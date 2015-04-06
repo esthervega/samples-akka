1c7b410c36622a652dabd643469adf8d0ee04836
esthervega <esther.vega@gmail.com>
2015-04-06 22:21:44 +0100

    had removed config

---
+++ presentation1/project/Dependencies.scala
@@ -0,0 +1,34 @@
+import sbt._
+
+object Version {
+  val akka         = "2.3.6"
+  val logback      = "1.1.2"
+  val scala        = "2.11.2"
+  val scalaParsers = "1.0.2"
+  val scalaTest    = "2.2.2"
+}
+
+object Library {
+  val akkaActor      = "com.typesafe.akka"      %% "akka-actor"               % Version.akka
+  val akkaSlf4j      = "com.typesafe.akka"      %% "akka-slf4j"               % Version.akka
+  val akkaTestkit    = "com.typesafe.akka"      %% "akka-testkit"             % Version.akka
+  val logbackClassic = "ch.qos.logback"         %  "logback-classic"          % Version.logback
+  val scalaParsers   = "org.scala-lang.modules" %% "scala-parser-combinators" % Version.scalaParsers
+  val scalaTest      = "org.scalatest"          %% "scalatest"                % Version.scalaTest
+  val akkaRemote     = "com.typesafe.akka"      % "akka-remote_2.11"          % Version.akka
+}
+
+object Dependencies {
+
+  import Library._
+
+  val presentation = List(
+    akkaActor,
+    akkaSlf4j,
+    logbackClassic,
+    scalaParsers,
+    akkaRemote,
+    akkaTestkit % "test",
+    scalaTest   % "test"
+  )
+}
